4636aa4d1d143859b5d40c447cad40d0295e8633
Nihal Gawale <nihalgawlae123@gmail.com>
2023-11-13 15:41:56 +0530

    testing api response

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const webpack = require("webpack");
 
-const { parsed: myEnv } = require("dotenv").config({
-  path: "/env",
-});
-
 const nextConfig = {
   // webpack(config) {
   //   config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
