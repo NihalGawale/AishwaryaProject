ca18932cee814b5b6660cd1a8ff9de79d60b40b0
Nihal Gawale <nihalgawlae123@gmail.com>
2023-11-13 15:49:36 +0530

    testing api routes

--- app/api/testing/route.js
+++ app/api/testing/route.js
@@ -1,8 +1,24 @@
+import connectToDB from "@/database/connection";
+import User from "@/models/users";
 import { NextResponse } from "next/server";
 
-export function GET (){
-  return  NextResponse.json({
-        success: false,
-        message: "Successfull",
-      })
+export async function GET (req){
+    await connectToDB();
+//   return  NextResponse.json({
+//         success: true,
+//         message: "Successfull",
+//       })
+const checkUser = await User.find()
+console.log(checkUser);
+if(checkUser) {
+    return NextResponse.json({
+        success: true,
+        data: checkUser, 
+      });
+}else{
+    return  NextResponse.json({
+                success: false,
+                message: "Failed",
+              })
+}
 }
